785b64d95140ef56d0a70b870ebf4ca35a846a13
Chad Payne <chadpayne@gmail.com>
2024-11-28 13:09:59 -0700

    added comments to the handle-command.js file

--- src/handle-command.js
+++ src/handle-command.js
@@ -1,19 +1,33 @@
+/**
+ * Processes a command and executes the corresponding action on the robot.
+ *
+ * @param {Robot} robot - The instance of the Robot class to operate on.
+ * @param {string} command - The command to execute. Valid commands are:
+ *   - 'L': Turn the robot left.
+ *   - 'R': Turn the robot right.
+ *   - 'M': Move the robot forward.
+ *   - '?': Display help/options.
+ * @param {string} options - A string containing the help menu or options to display for the '?' command.
+ */
 const handleCommand = (robot, command, options) => {
   switch (command) {
-    case 'L':
+    case 'L': // Command to turn the robot 90 degrees to the left.
       robot.turnLeft()
       break
-    case 'R':
+    case 'R': // Command to turn the robot 90 degrees to the right.
       robot.turnRight()
       break
-    case 'M':
+    case 'M': // Command to move the robot forward in its current direction.
       robot.moveForward()
       break
-    case '?':
+    case '?': // Command to display the help menu.
       console.log(options)
-      return
+      return // Exit early as there's no status update needed for help display.
   }
+
+  // Print the robot's current status after executing the command.
   console.log(robot.getStatus())
 }
 
+// Export the function to allow use in other parts of the application.
 export default handleCommand
